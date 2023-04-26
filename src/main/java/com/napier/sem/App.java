a2a50fd2deb7b56de8e1b020a59f37088ba50640
FraserUni <124901834+fraseruni@users.noreply.github.com>
2023-04-27 00:06:18 +0100

    Started adapting App queries to world database

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -3,21 +3,34 @@ package com.napier.sem;
 import java.sql.*;
 import java.util.ArrayList;
 
+/**
+ * Application
+ */
 public class App
 {
-    public static void main(String[] args)
-    {
-        // Create new Application
+    /**
+     * App Main Method
+     * @param args Database Location, Delay between Connection Attempts
+     */
+    public static void main(String[] args) {
+        // Create new Application and connect to database
         App a = new App();
 
-        // Connect to database
-        a.connect();
+        if(args.length < 1) {
+            a.connect("localhost:33060", 30000);
+        }
+        else {
+            a.connect(args[0], Integer.parseInt(args[1]));
+        }
 
         // Extract employee salary information
-        ArrayList<Employee> employees = a.getAllSalaries();
+        //ArrayList<Employee> employees = a.getAllSalaries();
+
+        // Extract city population information
+        ArrayList<City> cities = a.getAllPopulations();
 
         // Test the size of the returned data - should be 240124
-        System.out.println(employees.size());
+        //System.out.println(employees.size());
 
         // Disconnect from database
         a.disconnect();
@@ -30,10 +43,8 @@ public class App
 
     /**
      * Connect to the MySQL database.
-     * @void
      */
-    public void connect()
-    {
+    public void connect(String location, int delay) {
         try
         {
             // Load Database driver
@@ -49,18 +60,19 @@ public class App
         for (int i = 0; i < retries; ++i)
         {
             System.out.println("Connecting to database...");
-            try
-            {
+            try {
                 // Wait a bit for db to start
-                Thread.sleep(30000);
+                Thread.sleep(delay);
                 // Connect to database
-                con = DriverManager.getConnection("jdbc:mysql://db:3306/employees?useSSL=false", "root", "example");
+                con = DriverManager.getConnection("jdbc:mysql://" + location
+                                + "/world?allowPublicKeyRetrieval=true&useSSL=false",
+                        "root", "example");
                 System.out.println("Successfully connected");
                 break;
             }
             catch (SQLException sqle)
             {
-                System.out.println("Failed to connect to database attempt " + Integer.toString(i));
+                System.out.println("Failed to connect to database attempt " + (i));
                 System.out.println(sqle.getMessage());
             }
             catch (InterruptedException ie)
@@ -72,7 +84,6 @@ public class App
 
     /**
      * Disconnect from the MySQL database.
-     * @void
      */
     public void disconnect()
     {
@@ -91,9 +102,48 @@ public class App
     }
 
     /**
+     * Get City details.
+     * @return city, or null if there is an error.
+     */
+    public City getCity(int ID)
+    {
+        try
+        {
+            // Create an SQL statement
+            Statement stmt = con.createStatement();
+            // Create string for SQL statement
+            String strSelect =
+                    "SELECT city_id, name"
+                            + "FROM city"
+                            + "WHERE city_id = " + ID;
+            // Execute SQL statement
+            ResultSet rset = stmt.executeQuery(strSelect);
+            // Return new city if valid.
+            // Check one is returned
+            if (rset.next())
+            {
+                City city = new City();
+                city.city_id = rset.getInt("city_id");
+                city.name = rset.getString("name");
+                return city;
+            }
+            else
+                return null;
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get city details");
+            return null;
+        }
+    }
+
+    /*
+    /**
      * Get Employee details.
      * @return emp, or null if there is an error.
      */
+    /*
     public Employee getEmployee(int ID)
     {
         try
@@ -128,10 +178,30 @@ public class App
         }
     }
 
+     */
+
+
+    /**
+     * Display City details.
+     */
+    public void displayCity(City city)
+    {
+        if (city != null)
+        {
+            System.out.println(
+                    city.city_id + " "
+                            + city.name + " "
+                            + city.country_code + "\n"
+                            + city.district + "\n"
+                            + "Population:" + city.population + "\n");
+        }
+    }
+
+    /*
     /**
      * Display Employee details.
-     * @void
      */
+    /*
     public void displayEmployee(Employee emp)
     {
         if (emp != null)
@@ -147,10 +217,51 @@ public class App
         }
     }
 
+     */
+
+    /**
+     * Gets all the current cities and populations.
+     * @return A list of all cities and populations, or null if there is an error.
+     */
+    public ArrayList<City> getAllPopulations()
+    {
+        try
+        {
+            // Create an SQL statement
+            Statement stmt = con.createStatement();
+            // Create string for SQL statement
+            String strSelect =
+                    "SELECT cities.city_id, cities.name, cities.population"
+                            + "FROM cities "
+                            + "ORDER BY cities.city_id ASC";
+            // Execute SQL statement
+            ResultSet rset = stmt.executeQuery(strSelect);
+            // Extract employee information
+            ArrayList<City> cities = new ArrayList<>();
+            while (rset.next())
+            {
+                City city = new City();
+                city.city_id = rset.getInt("cities.city_id");
+                city.name = rset.getString("cities.name");
+                city.population = rset.getInt("cities.population");
+                cities.add(city);
+            }
+            return cities;
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get population details");
+            return null;
+        }
+    }
+
+    /*
     /**
      * Gets all the current employees and salaries.
      * @return A list of all employees and salaries, or null if there is an error.
      */
+    /*
     public ArrayList<Employee> getAllSalaries()
     {
         try
@@ -185,4 +296,5 @@ public class App
             return null;
         }
     }
+     */
 }
---
+++ src/main/java/com/napier/sem/City.java
@@ -0,0 +1,31 @@
+package com.napier.sem;
+
+/**
+ * Represents a city
+ */
+public class City {
+    /**
+     * City ID
+     */
+    public int city_id;
+
+    /**
+     * Name of city
+     */
+    public String name;
+
+    /**
+     * Country code of city
+     */
+    public String country_code;
+
+    /**
+     * District city resides within
+     */
+    public String district;
+
+    /**
+     * Population of city
+     */
+    public int population;
+}
